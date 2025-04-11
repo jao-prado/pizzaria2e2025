8058e6943fe35e886e5452e1e62604650f8b355e
jao-prado <rm95221@estudante.fieb.edu.br>
2025-04-11 14:08:54 +0000

    imagem poke

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,65 @@
-import{ use, useState} from "react"
+import { useState } from "react"
+import poke from './assets/pokeapi_256.png'
 
 function App(){
 
-  const [nome, setNome]= useState ('Ronaldo')
+  const [nome, setNome] = useState('Ronaldo')
   const [idade, setIdade] = useState(18)
-  const [time, setTime] = useState ('curintia')
-  const [pi] = useState (3.14)
+  const [time, setTime] = useState('Santos FC')
+  const [pi] = useState(3.14)
 
   //var idade = 18;
-  //let time = 'curintia';
+  //let time = 'Santos FC';
   //const pi = 3.14;
 
+  const Formulario = (props) => {
+    const [valor, setValor] = useState()
+    
+    return (
+      <div>
+        <input 
+          className = "nome"
+          placeholder= {props.sombra}
+          onChange={(e)=>{setValor(e.target.value)}}
+          type="text" />
 
-  const Formulario = (props)=> {
-    const[valor, setValor] = useState()
-      return (
-        <div>
-          <input
-            ClassName="nome"
-            placeholder= {props.sombra}
-            onChange={(e)=>{setNome(e.target.value)}}
-            type="text" />
-        
-      <button 
-        ClassName="botao"
-        onClick={()=> {
-          if(valor.length > 0)
-            alert(valor); 
-        
-          else 
-          alert(props.nome + ' idade é ' + props.idade)}}
-  
-      >
-            clique aqui
-          </button>
-        </div>
-      )
-    }
+        <button 
+          className="botao"
+          onClick={()=>{
+            if(valor.length > 0){
+              alert(valor);
+            }
+            else{
+              alert(props.nome + ' idade é ' + props.idade);
+            }
+            }}      
+        >
+          CLIQUE AQUI
+        </button>
+      </div>
+    )
+  }
 
-  return(
+
+  return (
     <div>
       <h3>Pizzaria 2E</h3>
-    
-    <Formulario nome="Ricardo"
-      idade={45}
-    sombra="digite eu nome..."/>
-    <Formulario nome="maria" sombra="Digite sua idade..."/>
-    <Formulario nome="tomas" sombra="Digite seu time..."/>
-    <Formulario nome="Antonieta" sombra="valor de pi..."/>
-   
-    
-      </div>
+
+      <img src={poke} style ={{width:400}}/>
+
+      <Formulario nome="Ricardo"
+          idade={45}
+          sombra="Digite nome..." />
+      <Formulario nome="Maria" sombra="Digite sua idade..."/>
+      <Formulario nome="Tomas" sombra="Digite seu time..." />
+      <Formulario nome="Antonieta" sombra="valor de pi" />
+      <Formulario />
+      <Formulario />
+      <Formulario />
+      <Formulario />
+    </div>
   )
 }
 
 export default App
+ 
