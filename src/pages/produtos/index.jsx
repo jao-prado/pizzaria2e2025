a388028554c94d55b3ab1267de5b38f6ad28a1dc
jao-prado <rm95221@estudante.fieb.edu.br>
2025-09-15 07:55:52 -0300

    listagem de produtos

---
+++ src/pages/produtos/index.jsx
@@ -0,0 +1,17 @@
+const Produtos = () => {
+
+    return(
+        <>
+        <h3>Listagem de Produtos</h3>
+        <ul>
+        <l1>Pizza de muçarela</l1>
+        <l1>Pizza de muçarela</l1>
+        <l1>Pizza de muçarela</l1>
+        <l1>Pizza de muçarela</l1>
+        <l1>Pizza de muçarela</l1>
+        
+      </ul>
+        </>
+    )
+
+}
